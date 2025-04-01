4b59550f055d346a2c65fd1c269d22d191a3bf55
fangrongg <developer@infigaro.com>
2025-04-01 15:42:47 +0800

    29 - unmatched routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -15,9 +15,9 @@ export default function DashboardLayout({
             <div>{children}</div>
             <div style={{ display: "flex"}}>
                 <div style={{ display: "flex", flexDirection: "column"}}>
-                <div>{users}</div>
-                <div>{revenue}</div>
-            </div>
+                    <div>{users}</div>
+                    <div>{revenue}</div>
+                </div>
             <div style={{ display: "flex", flex: 1}}>{notifications}</div>
             </div>
         </div>
