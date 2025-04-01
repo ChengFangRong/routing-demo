6ed891051704bb79e1a83342dfbfd5882eaf5312
fangrongg <developer@infigaro.com>
2025-04-01 14:00:31 +0800

    24 - error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,20 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);    
+}
+
 export default function ReviewDetails({params} : 
     {params: {
         productId: string, 
-        reviewId: string}
+        reviewId: string
+    };
 }) {
-
+    const random = getRandomInt(2)
+    if (random === 1) {
+        throw new Error("Error loading review");
+    }
+    
     if (parseInt(params.reviewId) > 1000) {
         notFound(); // 404 page
 }
