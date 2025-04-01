6816eb235dee7e26abaeafb6d87c92a27789bc47
fangrongg <developer@infigaro.com>
2025-04-01 12:24:05 +0800

    17 - routing metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,25 @@
+
+import { Metadata } from "next";
+
+export const generateMetadata = async ({ params }: Props): Promise <Metadata> => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`);
+        }, 100);
+    }
+    );
+   
+    return {
+        title: `Product ${title}`,
+    };
+}
+
+type Props = {
+    params: { productId: string };
+};
+
+
+
 export default function ProductDetail( {params} : { //params is the folder [productId]
     params: { productId: string };
 }) {
